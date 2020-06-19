481f70b51344c98712d2352f7defd718e0fcdbc9
lucas <you@example.com>
2020-06-19 17:09:11 +0200

    files modified

--- app/src/main/java/com/lucas/devprofile/MainActivity.kt
+++ app/src/main/java/com/lucas/devprofile/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.lucas.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawable
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        //Rounded an image with Type Float
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+
+//        write the line below if we want a Circular image
+//        rounded.isCircular = true
+        logo.setImageDrawable(rounded)
+
     }
 }
